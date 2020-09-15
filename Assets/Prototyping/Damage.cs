4adb12480c69372e5fe1f04b92f0a24904b72570
= <=>
2020-09-15 15:20:45 -0400

    Created damage class to hold all damage type info

---
+++ Assets/Prototyping/Damage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public enum DamageType
+{
+    none,       //bullets/mana
+    impact,     //hammers
+    peircing,   //spears
+    slash,      //swords
+    shatter,    //pickaxe
+}
+
+public class Damage
+{
+    public int damage;
+    public Element element;
+    public DamageType type;
+}
