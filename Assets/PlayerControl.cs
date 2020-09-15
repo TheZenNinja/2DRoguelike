ba5bedafa6e88f23c0cf783a42f72701c7b55632
Laptop <mcthebrickninja@gmail.com>
2020-09-15 16:37:05 -0400

    started work on platformer controls

--- Assets/PlayerControl.cs
+++ Assets/PlayerControl.cs
@@ -13,7 +13,11 @@ public class PlayerControl : MonoBehaviour
     public float speed = 5;
     public float sprintSpeed = 10;
     public float acceleration = 10;
-    public bool sprinting; 
+    public bool sprinting;
+    public float gravity = 20;
+    public float jumpForce = 20;
+    public bool grounded;
+    public LayerMask groundLayer;
 
     [Header("Dashing")]
     public float dashDistance = 5;
@@ -22,6 +26,8 @@ public class PlayerControl : MonoBehaviour
     public Timer dashCooldown;
 
     public static event System.Action onDash;
+
+    [SerializeField] Collider2D footCol;
 
     Vector2 velocity;
     Vector2 dashVelocity;
@@ -41,6 +47,8 @@ public class PlayerControl : MonoBehaviour
     }
     public void Move()
     {
+        grounded = footCol.IsTouchingLayers(groundLayer);
+
         inputDir = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
 
         if (Input.GetKeyDown(KeyCode.Space) && dashCooldown.finished)
@@ -48,7 +56,10 @@ public class PlayerControl : MonoBehaviour
 
         sprinting = Input.GetKey(KeyCode.LeftShift);
 
-        Vector2 goalVel = sprinting ? inputDir * sprintSpeed : inputDir * speed;
+        Vector2 goalVel = Vector2.zero;
+        goalVel.x = sprinting ? inputDir.x * sprintSpeed : inputDir.x * speed;
+        if (!grounded)
+            goalVel.y -= gravity * Time.deltaTime;
         velocity = Vector2.Lerp(velocity, goalVel, Time.deltaTime * acceleration);
 
         rb.velocity = velocity + dashVelocity;
